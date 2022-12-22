28eb6a150e2f5cab91fb046f41ed91b8f85ab359
isaka <isaka@MASSIVE>
2022-12-22 22:37:04 +0300

    for döngüsü

--- index.php
+++ index.php
@@ -1,5 +1,38 @@
-<?php 
 
-echo "ilk git projem";
+<?php
+
+//for döngüsü ile üçgen çizimi
+
+$n = 10;
+$symbol = "*";
+
+for ($i = 1; $i <= $n; $i++) {
+    echo '<br>';
+    for ($j = 1; $j <= $i; $j++) {
+        
+        echo $symbol;
+    
+    }
+ 
+}
+$echo = "das";
+
+//Çift sayıları bulan örnek
+
+$ciftsayilar = [];
+
+for($i=1; $i<=100; $i++){
+    if($i % 2 == 0 ){
+        $ciftsayilar[] = $i;
+    }
+}
+
+echo '<pre>';
+print_r($ciftsayilar);
+echo '</pre>';
+
+
+
+
 
 ?>
